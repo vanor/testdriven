4a9384851ccd28c2f29ee3bc126878060a155424
Guemkam Yvan <yvan.guemkam@worldvoice.cm>
2020-04-11 14:09:36 +0100

    reset des codes

--- README.md
+++
@@ -1,2 +0,0 @@
-# Microservices	with Docker, Flask, and React
-[![Build Status](https://travis-ci.org/YOUR_GITHUB_USERNAME/testdriven-app.svg?branch=master)](https://travis-ci.org/vanor/testdriven-app)
--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,36 +1,18 @@
-# services/users/project/__init.py
+# services/users/project/__init__.py
 
-import os
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_debugtoolbar import DebugToolbarExtension
-from flask_cors import CORS
+from flask import Flask, jsonify
 
 
-# instantiate the db and debug toolbar
-db = SQLAlchemy()
-toolbar = DebugToolbarExtension()
+# instantiate the app
+app = Flask(__name__)
+
+# set config
+app.config.from_object('project.config.DevelopmentConfig')
 
 
-def create_app(script_info=None):
-	# instanciate the app
-	app = Flask(__name__)
-
-	# enable CORS
-	CORS(app)
-
-	# set config
-	app_settings = os.getenv('APP_SETTINGS')
-	app.config.from_object(app_settings)
-
-	# set up extensions
-	db.init_app(app)
-	toolbar.init_app(app)
-
-	# register blueprints
-	from project.api.users import users_blueprint
-	app.register_blueprint(users_blueprint)
-
-	# shell context for flask cli
-	app.shell_context_processor({'app': app, 'db': db})
-	return app
+@app.route('/users/ping', methods=['GET'])
+def ping_pong():
+	return jsonify({
+		'status': 'success',
+		'message': 'pong!'
+	})
